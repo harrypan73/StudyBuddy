56b9ae1d0c1d4bd8d376cae01bb811eca4784414
Harry Pan <harry@Harrys-MacBook-Air.local>
2025-03-06 21:45:58 -0500

    Deleted temporary file, created simple screens for calendar, friends, and map

--- frontend/screens/CalendarScreen.js
+++ frontend/screens/CalendarScreen.js
@@ -1,4 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
 export default function CalendarScreen({ navigation }) {
-    
+    return (
+        <View style = { styles.container }>
+            <Text>Calendar Screen</Text>
+        </View>
+    );
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderColor: 'black',
+    },
+});
--- frontend/screens/MapScreen.js
+++ frontend/screens/MapScreen.js
@@ -1,3 +1,19 @@
-export default function MapScreen({ navigation }) {
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
+export default function MapScreen({ navigation }) {
+    return (
+        <View style = { styles.container }>
+            <Text>Map Screen</Text>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderColor: 'black',
+    },
+});

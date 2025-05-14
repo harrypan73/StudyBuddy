e293b9f1b9d56ff178ce497c745a74f0a8a72062
harrypan73 <harrypan73@gmail.com>
2025-05-14 11:44:41 -0400

    Added userRoutes. Not currently being used, but could be useful.

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 // Routes
 const authRoutes = require('./routes/authRoutes');
 const studySessionRoutes = require('./routes/studySessionRoutes');
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
@@ -24,7 +25,7 @@ mongoose.connect(process.env.MONGODB_URI)
 // Connect routes
 app.use('/api/auth', authRoutes);
 app.use('/api/study-sessions', studySessionRoutes);
-// app.use('/api/users', userRoutes);
+app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
